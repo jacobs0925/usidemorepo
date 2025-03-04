98fc70b9fbaad5f6900b0f7866c584efc4db8985
jacobs0925 <jjschwar@uci.edu>
2025-03-03 21:16:27 -0800

    added mobile nav

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,6 +3,7 @@ import '../assets/css/NavBar.css'
 import logo from '../assets/logo.png'
 import { Link } from 'react-router-dom'
 import { RxHamburgerMenu } from "react-icons/rx";
+import { useState } from 'react';
 
 const NavBarButton = ({ text, nav }) =>
 {
@@ -15,31 +16,30 @@ const NavBarButton = ({ text, nav }) =>
 
 const NavBar = () =>
 {
+    const navButtons = <div className='navButtonContainer'>
+        <NavBarButton text={'Home'} nav={'/'} />
+        <NavBarButton text={'Location & Hours'} nav={'/location'} />
+        <NavBarButton text={'Ammunition'} nav={'/ammunition'} />
+        <NavBarButton text={'Membership'} nav={'/membership'} />
+        <NavBarButton text={'Calendar'} nav={'/calendar'} />
+        <NavBarButton text={'Instructors'} nav={'/instructors'} />
+        <NavBarButton text={'News'} nav={'/news'} />
+        <NavBarButton text={'Wavier'} nav={'/waiver'} />
+        <NavBarButton text={'Contact Us'} nav={'/contact'} />
+    </div>
+    const [mobileNavVisible, revealNav] = useState(false)
     return (
         <>
             <div className="navmobilecontainer">
-                <RxHamburgerMenu className='burger' color={'white'} />
+                <RxHamburgerMenu onClick={() => revealNav(!mobileNavVisible)} className='burger' color={'white'} />
+                {mobileNavVisible ? navButtons : null}
             </div>
             <div className='nav-container'>
                 <div className='navlogocontainer'>
                     <img className="navlogo" src={logo} alt='logo'></img>
                     <div className='navlogotext'>United Sportsmen</div>
                 </div>
-                <div className='navButtonContainer'>
-                    <NavBarButton text={'Home'} nav={'/'} />
-                    <NavBarButton text={'Location & Hours'} nav={'/location'} />
-                    <NavBarButton text={'Ammunition'} nav={'/ammunition'} />
-                    <NavBarButton text={'Membership'} nav={'/membership'} />
-                    <NavBarButton text={'Calendar'} nav={'/calendar'} />
-                    <NavBarButton text={'Instructors'} nav={'/instructors'} />
-                    <NavBarButton text={'News'} nav={'/news'} />
-                    <NavBarButton text={'Wavier'} nav={'/waiver'} />
-                    <NavBarButton text={'Contact Us'} nav={'/contact'} />
-                </div>
-                {/* <div className='navButtonContainer'>
-                <VedusButton text='Click Here' onPress={() => console.log('aaaaa')}></VedusButton>
-                <VedusButton text='Sign Up' invert={true} onPress={() => console.log('bbbbb')}></VedusButton>
-            </div> */}
+                {navButtons}
             </div>
         </>
 
